b93b7a08d0f1ec05a93a390014cdd95277aba997
CodeMasterMitesh <prajapati.mitesh461@gmail.com>
2025-07-11 08:43:05 +0530

    v1

---
+++ basic.js
@@ -0,0 +1,45 @@
+// alert("JavaScript");
+
+// console.log("Good Morning");
+
+// course = "JavaScript Basic";
+
+// console.log(course);
+
+// name = "Mitesh";
+// num = 25;
+// num1 = 26;
+
+// console.log(num + num1);
+
+
+
+var x = 25; 
+var x = 26;
+
+var x = 250;
+
+console.log(x);
+// ECMA Script 6
+// let , const 
+
+let y = 98;
+y = 68;
+
+{
+    let y = 15;
+    console.log(y);
+}
+console.log(y);
+
+
+const z = 75;
+// z = 65;
+
+
+{
+    const z = 24;
+    console.log("z = " + z);
+}
+
+console.log(z);
